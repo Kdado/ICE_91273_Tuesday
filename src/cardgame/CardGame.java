3ce40dd910db6952b7cdc5ebdc800db6ed732bb6
Kdado <dadok@sheridancollege.ca>
2019-09-24 20:53:58 -0400

    ICE 1 Starter Code Kdado

---
+++ src/cardgame/CardGame.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cardgame;
+
+/**
+ *
+ * @author Revenger Mini
+ */
+public class CardGame {
+
+  private String suit; // spades, diamonds, clubs, hearts
+  private int value; // 1 to 13 
+  public static final String[] Suits = {"clubs", "hearts", "diamonds", "spades"};
+
+    /**
+     * @return the suit
+     */
+    public String getSuit() {
+        return suit;
+    }
+
+    /**
+     * @param suit the suit to set
+     */
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+//2 methods for random generation of suits and value 
+    public int ranSuit(){
+        int value = (int)(Math.random()*3)+0;
+        return value;
+    }
+    public int ranValue(){
+        int value = (int) (Math.random()*13)+1;
+        return value;
+    }
+}
